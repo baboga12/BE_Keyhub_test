ff27a85ef9dbd151907e3cf4cf521c97ba2d1263
unknown <ngochai06122002@gmail.com>
2024-05-12 20:24:09 +0700

    DueDate

--- services/adminService.js
+++ services/adminService.js
@@ -59,14 +59,14 @@ class AdminService{
             const dueDate = new Date();
             dueDate.setHours(23, 59, 59, 999);
 
-            // Điều chỉnh thời gian cho múi giờ địa phương
-            const offset = dueDate.getTimezoneOffset() * 60000; // Chuyển đổi múi giờ sang milliseconds
-            const localDueDate = new Date(dueDate - offset); // Trừ đi múi giờ
+            // // Điều chỉnh thời gian cho múi giờ địa phương
+            // const offset = dueDate.getTimezoneOffset() * 60000; // Chuyển đổi múi giờ sang milliseconds
+            // const localDueDate = new Date(dueDate - offset); // Trừ đi múi giờ
 
             // Thêm trường dueDate vào mỗi đối tượng setting
             return {
                 ...setting.toObject(), // Chuyển document Mongoose thành plain object
-                dueDate: localDueDate.toISOString() // Định dạng ISO 8601
+                dueDate: dueDate.toISOString() // Định dạng ISO 8601
             };
         });
         return results;
