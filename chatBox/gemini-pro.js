80d09de4f959c42c3ee6e388a9a88dd99e72aa5f
unknown <ngochai06122002@gmail.com>
2024-05-20 13:48:49 +0700

    AI fix

--- chatBox/gemini-pro.js
+++ chatBox/gemini-pro.js
@@ -10,12 +10,12 @@ const genAI = new GoogleGenerativeAI(apiKey); // Create GoogleGenerativeAI insta
 const run = async (req,res) => {
   try {
     const prompt = req.body.prompt;
-    console.log(prompt);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    console.log(text);
+    console.log("Generative model generated");
+    console.log('--------------------------------------------------------------------------------------------------------------------');
     return res.status(200).json(
         {
             success: true,
